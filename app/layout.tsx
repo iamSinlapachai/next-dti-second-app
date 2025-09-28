35bf5e881086fdf26d261fa08b1dd1584df0168d
iamSinlapachai <sinlapachai.kaeobunrueang@gmail.com>
2025-09-28 17:16:46 +0700

    Edit student page and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import MenuBar from "@/components/MenuBar";
+//import MenuBar from "@/components/MenuBar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- app/student/[id]/page.tsx
+++ app/student/[id]/page.tsx
@@ -1,5 +1,5 @@
-export default function Student({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default async function Student({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
   return (
     <main>
       <h1>Student ID: {id}</h1>
